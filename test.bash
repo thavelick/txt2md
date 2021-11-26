78fe37a6c91cc78f90f6a7063a8c07dfed30a674
Tristan Havelick <tristan@havelick.com>
2021-11-26 11:37:42 -0700

    test: add new rfc example, make lorem test more standard

--- test.bash
+++ test.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 diff examples/lorem/expected_unwrap.txt <(./unwrap.py examples/lorem/input.txt)
-diff expamles/lorem/input.txt <(./txt2md examples/lorem/input.txt)
+diff examples/lorem/expected_out.txt <(./txt2md.py examples/lorem/input.txt)
+diff examples/rfc/expected_out.txt <(./txt2md.py examples/rfc/input.txt)
